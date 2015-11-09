11c52c75425cfb01c287d61c108a07fa447a5bf8
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-11-10 00:24:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,236 @@
+#!/usr/bin/python
+
+import json
+import sys
+import os
+import math
+
+from PIL import Image
+
+supportedExtensions = set([".PNG", ".TGA", ".PPM"])
+
+modeToBitsPerPixel = {'1':1, 'L':8, 'P':8, 'RGB':24, 'RGBA':32, 'CMYK':32, 'YCbCr':24, 'I':32, 'F':32}
+modeToBytesPerPixel = {'1':1.0/8.0, 'L':1, 'P':1, 'RGB':3, 'RGBA':4, 'CMYK':4, 'YCbCr':3, 'I':4, 'F':4}
+
+pwdStack = []
+def PushPWD(path):
+    pwdStack.append(os.getcwd());
+    os.chdir(path);
+
+def PopPWD():
+    os.chdir(pwdStack.pop())
+
+def TranslatePath(path, configPath):
+    if path.upper() == "@HERE":
+        pathDir = os.path.dirname(configPath)
+        if pathDir == "":
+            return "."
+        else:
+            return pathDir
+    else:
+        return path
+
+def mkdirFile(filename):
+    folder=os.path.dirname(filename)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def Byteify(input):
+    if isinstance(input, dict):
+        return {Byteify(key):Byteify(value) for key,value in input.iteritems()}
+    elif isinstance(input, list):
+        return [Byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+def PrettyBytes(nbytes):
+    if nbytes == 0: return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
+def IntWithCommas(x):
+    if type(x) not in [type(0), type(0L)]:
+        raise TypeError("Parameter must be an integer.")
+    if x < 0:
+        return '-' + intWithCommas(-x)
+    result = ''
+    while x >= 1000:
+        x, r = divmod(x, 1000)
+        result = ",%03d%s" % (r, result)
+    return "%d%s" % (x, result)
+
+def Execute(configPath):
+    print("Config file: " + configPath)
+    with open(configPath) as configJsonFile:
+        configJson = Byteify(json.load(configJsonFile));
+
+        if not "global" in configJson:
+            print("Error: No global field!")
+            return 2
+
+        if not "dev-screen-width" in configJson["global"]:
+            print("Error: No global::dev-screen-width specified!")
+            return 2
+        developmentScreenWidth = configJson["global"]["dev-screen-width"]
+        if developmentScreenWidth <= 0:
+            print("Error: Invalid global::dev-screen-width value!")
+            return 2
+
+        if not "dev-screen-height" in configJson["global"]:
+            print("Error: No global::dev-screen-height specified!")
+            return 2
+        developmentScreenHeight = configJson["global"]["dev-screen-height"]
+        if developmentScreenHeight <= 0:
+            print("Error: Invalid global::dev-screen-height value!")
+            return 2
+
+        if not "targets" in configJson:
+            print("Error: no targets field!")
+            return 2
+
+        if not "folders" in configJson:
+            print("Error: No folders to search for files")
+            return 2
+
+        if not "result" in configJson:
+            print("Error: no result field!");
+            return 2
+
+        if "pwd" in configJson:
+            PushPWD(TranslatePath(configJson["pwd"], configPath))
+
+        for path in configJson["exec"]:
+            Execute(path);
+
+
+        # Get all supported files from folders
+        filesData = {}
+        for folder in configJson["folders"]:
+            for root, subDirs, files in os.walk(os.path.join(os.getcwd(),folder)):
+                for filename in files:
+                    filePath = os.path.join(root, filename)
+
+                    filePathAbs = os.path.abspath(filePath);
+
+                    extension = os.path.splitext(filePathAbs)[1]
+
+                    if extension.upper() in supportedExtensions:
+                        image = Image.open(filePathAbs)
+                        filesData[filePathAbs] = {"save-to" : folder,
+							"real-width" : image.size[0],
+							"real-height" : image.size[1],
+							"bpp" : modeToBytesPerPixel[image.mode]}
+
+        # Update files with "tex-info" structure properties
+        if "tex-info" in configJson["global"]:
+            for filePath, texInfo in configJson["global"]["tex-info"].iteritems():
+                filePathAbs = os.path.abspath(filePath);
+                if filePathAbs in filesData:
+                    filesData[filesPathAbs].update(texInfo)
+                else:
+                    print("Specified file '%s' is either not found or unsupported" % (filePath))
+
+        targetsData = {}
+        cmds = []
+        for target in configJson["targets"]:
+            if not "name" in target:
+                print("Error: target name field not specified")
+                continue
+            targetName = target["name"]
+            if not "width" in target:
+                print("Error: target '%s' does not have width specified" % (targetName))
+                continue
+            targetWidth = target["width"]
+            if not "height" in target:
+                print("Error: target '%s' does not have height specified" % (targetName))
+                continue
+            targetHeight = target["height"]
+            if not "cmds" in target:
+                print("Error: target '%s' does not have a cmd list specified" % (targetName))
+                continue
+
+            targetsData[targetName] = {"pixel-count-after" : 0, "pixel-count-before" : 0, "bytes-count-after" : 0, "bytes-count-before" : 0}
+            for filePath, fileData in filesData.iteritems():
+                for func in target["cmds"]:
+                    cmd = {}
+                    cmd["func"] = func
+                    cmd["path"] = filePath
+
+                    cmd["real-width"] = fileData["real-width"]
+                    cmd["real-height"] = fileData["real-height"]
+
+                    if not "dev-width" in fileData:
+                        cmd["dev-width"] = cmd["real-width"]
+                    else:
+                        cmd["dev-width"] = fileData["dev-width"]
+
+                    if not "dev-height" in fileData:
+                        cmd["dev-height"] = cmd["real-height"]
+                    else:
+                        cmd["dev-height"] = fileData["dev-height"]
+
+                    cmd["new-width"] = math.floor( 1.0 * cmd["dev-width"] / developmentScreenWidth * targetWidth)
+                    cmd["new-height"] = math.floor( 1.0 * cmd["dev-height"] / developmentScreenHeight * targetHeight)
+
+                    if cmd["new-width"] > cmd["real-width"]:
+                        cmd["new-width"] = cmd["real-width"]
+
+                    if cmd["new-height"] > cmd["real-height"]:
+                        cmd["new-height"] = cmd["real-height"]
+
+                    targetsData[targetName]["pixel-count-after"] += cmd["new-width"] * cmd["new-height"]
+                    targetsData[targetName]["pixel-count-before"] += cmd["real-width"] * cmd["real-height"]
+
+                    targetsData[targetName]["bytes-count-after"] += cmd["new-width"] * cmd["new-height"] * fileData["bpp"]
+                    targetsData[targetName]["bytes-count-before"] += cmd["real-width"] * cmd["real-height"] * fileData["bpp"]
+
+                    cmd["path-prefix"] = targetName
+                    cmds.append(cmd)
+
+        for cmd in cmds:
+            pathPrefix = os.path.abspath(cmd["path-prefix"])
+            whereTo = os.path.join(pathPrefix,os.path.relpath(cmd["path"]))
+            mkdirFile(whereTo)
+            print("Target: " + cmd["path-prefix"] + " --> " + cmd["func"] + ": " + os.path.relpath(cmd["path"], scriptPath))
+
+        for targetName, targetData in targetsData.iteritems():
+            print("-------------------------------------------------------")
+            print("Target: %s" % targetName)
+            print("pixel-count-before: %d" % targetData["pixel-count-before"])
+            print("pixel-count-after: %d" % targetData["pixel-count-after"])
+            print("bytes-count-before: %s (%s)" %
+				(IntWithCommas(int(targetData["bytes-count-before"])),
+				PrettyBytes(targetData["bytes-count-before"])) )
+            print("bytes-count-after: %s (%s)" %
+				(IntWithCommas(int(targetData["bytes-count-after"])),
+				PrettyBytes(targetData["bytes-count-after"])) )
+            print("Saved memory: %s (%.2f%%)" %
+				(PrettyBytes(targetData["bytes-count-before"] -
+				targetData["bytes-count-after"]),
+				100.0 *  (targetData["bytes-count-before"] -
+				targetData["bytes-count-after"]) / targetData["bytes-count-before"]) )
+
+        if "pwd" in configJson:
+            PopPWD();
+        return 0
+    print("Error: Can not open: %s" % (configPath));
+    return 2
+
+def Main():
+    if len(sys.argv) != 2:
+        print("Usage: " + sys.argv[0] + " <path/to/config.json>")
+        sys.exit(2)
+
+    global scriptPath
+    scriptPath = os.getcwd()
+
+    sys.exit(Execute(sys.argv[1]))
+
+Main()
